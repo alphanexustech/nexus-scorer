57f5b052b17c92672b99ec993546d0cf14102dad
Nathaniel Buechler <natebuechler@gmail.com>
2018-01-05 14:01:18 -0500

    chore: Add initial boilerplate

---
+++ app/runserver.py
@@ -0,0 +1,10 @@
+from app import app
+import sys
+
+# Sets the port, or defaults to 80
+if (len(sys.argv) > 1):
+    port = int(sys.argv[1])
+else:
+    port=80
+
+app.run(debug=True, host='127.0.0.1', port=port)
